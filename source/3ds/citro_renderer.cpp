ceb29e3741b476c63186e4875fe9b7201f0bda3d
pyroticinsanity <this_can_only_be_mine@hotmail.com>
2020-04-01 18:11:49 +0000

    Cleaning up code and updating version

--- source/3ds/citro_renderer.cpp
+++ source/3ds/citro_renderer.cpp
@@ -11,16 +11,14 @@
 
 #include "citro_renderer.h"
 
-extern OS_Link                oslink;
-extern Viewer		viewer;
-extern Coordinate     crd;
+extern OS_Link oslink;
+extern Viewer viewer;
+extern Coordinate crd;
 
 struct Triangle
 {
 	Triangle(float X0, float Y0, float X1, float Y1, float X2, float Y2, u32 Color, Layers Layer)
-	: x0(X0), y0(Y0), x1(X1), y1(Y1), x2(X2), y2(Y2), color(Color), layer(Layer)
-	{
-	};
+		: x0(X0), y0(Y0), x1(X1), y1(Y1), x2(X2), y2(Y2), color(Color), layer(Layer){};
 	float x0;
 	float y0;
 	float x1;
@@ -33,48 +31,48 @@ struct Triangle
 
 std::queue<Triangle> Triangles;
 
-const struct kbdKey keyboardKeys[40] = { 			{87, 105, 15, 15, 'Q'},
-													{107, 105, 15, 15, 'W'},
-													{127, 105, 15, 15, 'E'},
-													{144, 105, 15, 15, 'R'},
-													{163, 105, 15, 15, 'T'},
-													{183, 105, 15, 15, 'Y'},
-													{201, 105, 15, 15, 'U'},
-													{221, 105, 15, 15, 'I'},
-													{240, 105, 15, 15, 'O'},
-													{259, 105, 15, 15, 'P'},
+const struct kbdKey keyboardKeys[40] = {{87, 105, 15, 15, 'Q'},
+										{107, 105, 15, 15, 'W'},
+										{127, 105, 15, 15, 'E'},
+										{144, 105, 15, 15, 'R'},
+										{163, 105, 15, 15, 'T'},
+										{183, 105, 15, 15, 'Y'},
+										{201, 105, 15, 15, 'U'},
+										{221, 105, 15, 15, 'I'},
+										{240, 105, 15, 15, 'O'},
+										{259, 105, 15, 15, 'P'},
 
-													{92, 128, 15, 15, 'A'},
-													{110, 128, 15, 15, 'S'},
-													{130, 128, 15, 15, 'D'},
-													{150, 128, 15, 15, 'F'},
-													{169, 128, 15, 15, 'G'},
-													{188, 128, 15, 15, 'H'},
-													{207, 128, 15, 15, 'J'},
-													{225, 128, 15, 15, 'K'},
-													{246, 128, 15, 15, 'L'},
-													{285, 128, 25, 15, 0x0D},
+										{92, 128, 15, 15, 'A'},
+										{110, 128, 15, 15, 'S'},
+										{130, 128, 15, 15, 'D'},
+										{150, 128, 15, 15, 'F'},
+										{169, 128, 15, 15, 'G'},
+										{188, 128, 15, 15, 'H'},
+										{207, 128, 15, 15, 'J'},
+										{225, 128, 15, 15, 'K'},
+										{246, 128, 15, 15, 'L'},
+										{285, 128, 25, 15, 0x0D},
 
-													{99,  150, 15, 15, 'Z'},
-													{119,  150, 15, 15, 'X'},
-													{139,  150, 15, 15, 'C'},
-													{159,  150, 15, 15, 'V'},
-													{178,  150, 15, 15, 'B'},
-													{197,  150, 15, 15, 'N'},
-													{217,  150, 15, 15, 'M'},
-													{237,  150, 15, 15, ','},
-													{257,  150, 15, 15, '.'},
-													{125,  173, 150, 15, ' '}
+										{99, 150, 15, 15, 'Z'},
+										{119, 150, 15, 15, 'X'},
+										{139, 150, 15, 15, 'C'},
+										{159, 150, 15, 15, 'V'},
+										{178, 150, 15, 15, 'B'},
+										{197, 150, 15, 15, 'N'},
+										{217, 150, 15, 15, 'M'},
+										{237, 150, 15, 15, ','},
+										{257, 150, 15, 15, '.'},
+										{125, 173, 150, 15, ' '}
 
-												};
+};
 
 const int CitroRenderer::ScreenHeight = 240;
 const int CitroRenderer::ScreenWidth = 400;
 
- CitroRenderer::CitroRenderer()
- 	: Renderer(), _currentImage(NULL), _xOffset(0), _yOffset(0)
+CitroRenderer::CitroRenderer()
+	: Renderer(), _currentImage(NULL), _xOffset(0), _yOffset(0)
 {
-		// Init libs
+	// Init libs
 	gfxInitDefault();
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
@@ -88,11 +86,11 @@ const int CitroRenderer::ScreenWidth = 400;
 	_right = C2D_CreateScreenTarget(GFX_TOP, GFX_RIGHT);
 
 	_keyboardImg.tex = new C3D_Tex();
-	Tex3DS_SubTexture* subtex = new Tex3DS_SubTexture();
+	Tex3DS_SubTexture *subtex = new Tex3DS_SubTexture();
 
 	subtex->left = 0;
-	subtex->top = 0; 
-	subtex->width = 400; 
+	subtex->top = 0;
+	subtex->width = 400;
 	subtex->height = 240;
 	subtex->right = 1;
 	subtex->bottom = 1;
@@ -100,20 +98,20 @@ const int CitroRenderer::ScreenWidth = 400;
 	_keyboardImg.subtex = subtex;
 
 	Tex3DS_Texture t3x = Tex3DS_TextureImport(keyboard_t3x, keyboard_t3x_size,
-		 _keyboardImg.tex, NULL, false);
+											  _keyboardImg.tex, NULL, false);
 
 	// Delete the t3x object since we don't need it
 	Tex3DS_TextureFree(t3x);
 
-	printf("3DS Dungeons of Daggorath - 0.7\n");
+	printf("3DS Dungeons of Daggorath - 0.8\n");
 	printf("---------------------------------------\n");
 	printf("Controls:\n");
 	printf("D-Pad Up - MOVE UP\n");
 	printf("D-Pad Down - MOVE DOWN\n");
 	printf("D-Pad Left - MOVE LEFT\n");
 	printf("D-Pad Right - MOVE RIGHT\n");
-	printf("A - Custom Command\n");
-	printf("B - ATTACK RIGHT\n");
+	printf("B - Custom Command\n");
+	printf("A - ATTACK RIGHT\n");
 	printf("Y - ATTACK LEFT\n");
 	printf("X - EXAMINE / LOOK\n");
 	printf("L - Command Creator\n");
@@ -123,7 +121,7 @@ const int CitroRenderer::ScreenWidth = 400;
 }
 
 void CitroRenderer::beginRendering()
-{	
+{
 	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 	C2D_SceneBegin(_left);
 }
@@ -151,21 +149,23 @@ void CitroRenderer::drawLine(float x0, float y0, float x1, float y1, Layers laye
 	float yWidth = width * sin(atan(slope));
 	float xWidth = width * cos(atan(slope));
 
-	Triangles.push(Triangle(x0 - xWidth, y0 + yWidth,x0 + xWidth, y0 - yWidth,x1 - xWidth, y1 + yWidth, _color, layer));
+	Triangles.push(Triangle(x0 - xWidth, y0 + yWidth, x0 + xWidth, y0 - yWidth, x1 - xWidth, y1 + yWidth,
+							_color, layer));
 
-	C2D_DrawTriangle(x0 - xWidth, y0 + yWidth, _color, 
-		x0 + xWidth, y0 - yWidth, _color,
-		x1 - xWidth, y1 + yWidth, _color, 0);
+	C2D_DrawTriangle(x0 - xWidth, y0 + yWidth, _color,
+					 x0 + xWidth, y0 - yWidth, _color,
+					 x1 - xWidth, y1 + yWidth, _color, 0);
 
-	Triangles.push(Triangle(x1 - xWidth, y1 + yWidth,x0 + xWidth, y0 - yWidth,x1 + xWidth, y1 - yWidth, _color, layer));
+	Triangles.push(Triangle(x1 - xWidth, y1 + yWidth, x0 + xWidth, y0 - yWidth, x1 + xWidth, y1 - yWidth,
+							_color, layer));
 
-	C2D_DrawTriangle(x1 - xWidth, y1 + yWidth, _color, 
-		x0 + xWidth, y0 - yWidth, _color,
-		x1 + xWidth, y1 - yWidth, _color, 0);
+	C2D_DrawTriangle(x1 - xWidth, y1 + yWidth, _color,
+					 x0 + xWidth, y0 - yWidth, _color,
+					 x1 + xWidth, y1 - yWidth, _color, 0);
 }
 
 void CitroRenderer::drawQuad(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3,
-							Layers layer)
+							 Layers layer)
 {
 	// TODO: Flip the y-axis for some reason.
 	y0 = ScreenHeight - y0;
@@ -173,19 +173,19 @@ void CitroRenderer::drawQuad(float x0, float y0, float x1, float y1, float x2, f
 	y2 = ScreenHeight - y2;
 	y3 = ScreenHeight - y3;
 
-	Triangles.push(Triangle(_xOffset + x0, _yOffset + y0, _xOffset + x1, _yOffset + y1, _xOffset + x2, _yOffset + y2, 
-		_color, layer));
+	Triangles.push(Triangle(_xOffset + x0, _yOffset + y0, _xOffset + x1, _yOffset + y1, _xOffset + x2,
+							_yOffset + y2, _color, layer));
 
-	C2D_DrawTriangle(_xOffset + x0, _yOffset + y0, _color, 
-		_xOffset + x1, _yOffset + y1, _color,
-		_xOffset + x2, _yOffset + y2, _color, 0);
+	C2D_DrawTriangle(_xOffset + x0, _yOffset + y0, _color,
+					 _xOffset + x1, _yOffset + y1, _color,
+					 _xOffset + x2, _yOffset + y2, _color, 0);
 
-	Triangles.push(Triangle(_xOffset + x2, _yOffset + y2, _xOffset + x3, _yOffset + y3, _xOffset + x0, _yOffset + y0,
-		 _color, layer));
-	
-	C2D_DrawTriangle(_xOffset + x2, _yOffset + y2, _color, 
-		_xOffset + x3, _yOffset + y3, _color,
-		_xOffset + x0, _yOffset + y0, _color, 0);
+	Triangles.push(Triangle(_xOffset + x2, _yOffset + y2, _xOffset + x3, _yOffset + y3, _xOffset + x0,
+							_yOffset + y0, _color, layer));
+
+	C2D_DrawTriangle(_xOffset + x2, _yOffset + y2, _color,
+					 _xOffset + x3, _yOffset + y3, _color,
+					 _xOffset + x0, _yOffset + y0, _color, 0);
 }
 
 void CitroRenderer::drawKeyboard(struct kbdKey key)
@@ -200,35 +200,38 @@ void CitroRenderer::drawKeyboard(struct kbdKey key)
 
 	_currentImage = &_keyboardImg;
 
-  	resetMatrix();
+	resetMatrix();
 
 	setColor(0.0f, 1.0f, 0.0f);
-	drawLine(highlightX, highlightY, highlightX + squareWidth, highlightY, LAYER_UI);
-	drawLine(highlightX + squareWidth, highlightY, highlightX + squareWidth, highlightY + squareHeight, LAYER_UI);
-	drawLine(highlightX + squareWidth, highlightY + squareHeight, highlightX, highlightY + squareHeight, LAYER_UI);
-	drawLine(highlightX, highlightY + squareHeight, highlightX, highlightY, LAYER_UI);
+	drawLine(highlightX, highlightY, highlightX + squareWidth, highlightY);
+	drawLine(highlightX + squareWidth, highlightY, highlightX + squareWidth, highlightY + squareHeight);
+	drawLine(highlightX + squareWidth, highlightY + squareHeight, highlightX, highlightY + squareHeight);
+	drawLine(highlightX, highlightY + squareHeight, highlightX, highlightY);
 	setColor(viewer.fgColor);
 }
 
 void CitroRenderer::drawVector(float X0, float Y0, float X1, float Y1, Layers layer)
 {
-    	if (g_options&OPT_VECTOR) {
-		float		clrLine[3];
+	if (g_options & OPT_VECTOR)
+	{
+		float clrLine[3];
 
-		if (viewer.VCTFAD == 0xff) return; // do not draw lines with VCTFAD=255
+		if (viewer.VCTFAD == 0xff)
+			return; // do not draw lines with VCTFAD=255
 
 		// calculate line color from VCTFAD
-		float flBirghtness=1.0f/(viewer.VCTFAD/2.0f + 1.0f);
+		float flBirghtness = 1.0f / (viewer.VCTFAD / 2.0f + 1.0f);
 		// calculate color between FG and BG
-		clrLine[0]=viewer.fgColor[0]*flBirghtness+viewer.bgColor[0]*(1.0f-flBirghtness);
-		clrLine[1]=viewer.fgColor[1]*flBirghtness+viewer.bgColor[1]*(1.0f-flBirghtness);
-		clrLine[2]=viewer.fgColor[2]*flBirghtness+viewer.bgColor[2]*(1.0f-flBirghtness);
+		clrLine[0] = viewer.fgColor[0] * flBirghtness + viewer.bgColor[0] * (1.0f - flBirghtness);
+		clrLine[1] = viewer.fgColor[1] * flBirghtness + viewer.bgColor[1] * (1.0f - flBirghtness);
+		clrLine[2] = viewer.fgColor[2] * flBirghtness + viewer.bgColor[2] * (1.0f - flBirghtness);
 
-        setColor(clrLine[0], clrLine[1], clrLine[2]);
+		setColor(clrLine[0], clrLine[1], clrLine[2]);
 		drawLine(X0, Y0, X1, Y1, layer);
 		setColor(viewer.fgColor);
 	}
-	else {
+	else
+	{
 		float XL, YL, L;
 		int FADCNT;
 		double DX, DY, XX, YY;
@@ -245,15 +248,16 @@ void CitroRenderer::drawVector(float X0, float Y0, float X1, float Y1, Layers la
 		{
 			return;
 		}
-		DX = ((double) XL / (double) L) * ((X0 < X1) ? 1 : -1);
-		DY = ((double) YL / (double) L) * ((Y0 < Y1) ? 1 : -1);
-		if (g_options&OPT_HIRES) { // prepare to draw a HIRES line
+		DX = ((double)XL / (double)L) * ((X0 < X1) ? 1 : -1);
+		DY = ((double)YL / (double)L) * ((Y0 < Y1) ? 1 : -1);
+		if (g_options & OPT_HIRES)
+		{ // prepare to draw a HIRES line
 			// in hires mode, all we need to do is increase the # pixels per line.
 			// we are scaling up according to the screen width
-			int scale=oslink.width>>8;
-			DX/=(double)scale;
-			DY/=(double)scale;
-			L*=scale;
+			int scale = oslink.width >> 8;
+			DX /= (double)scale;
+			DY /= (double)scale;
+			L *= scale;
 		}
 		XX = X0 + 0.5;
 		YY = Y0 + 0.5;
@@ -265,9 +269,10 @@ void CitroRenderer::drawVector(float X0, float Y0, float X1, float Y1, Layers la
 				if (XX >= 0.0 && XX < 256.0 &&
 					YY >= 0.0 && YY < 152.0)
 				{
-					if (g_options&OPT_HIRES)
+					if (g_options & OPT_HIRES)
 						plotPoint(XX, YY, layer);
-					else {
+					else
+					{
 						plotPoint((int)XX, (int)YY, layer);
 					}
 				}
@@ -288,62 +293,61 @@ void CitroRenderer::endRendering()
 
 void CitroRenderer::initialize()
 {
-
 }
 
 void CitroRenderer::plotPoint(double X, double Y, Layers layer)
 {
 	drawQuad(
-			_xOffset + crd.newX(X),	_yOffset + crd.newY(Y),
-			_xOffset + crd.newX(X+1),	_yOffset + crd.newY(Y),
-			_xOffset + crd.newX(X+1),	_yOffset + crd.newY(Y+1),
-			_xOffset + crd.newX(X),	_yOffset + crd.newY(Y+1), layer);
+		_xOffset + crd.newX(X), _yOffset + crd.newY(Y),
+		_xOffset + crd.newX(X + 1), _yOffset + crd.newY(Y),
+		_xOffset + crd.newX(X + 1), _yOffset + crd.newY(Y + 1),
+		_xOffset + crd.newX(X), _yOffset + crd.newY(Y + 1), layer);
 }
 
 void CitroRenderer::renderRightScreen()
 {
-	if(Triangles.size() > 0)
+	if (Triangles.size() > 0)
 	{
 		C2D_SceneBegin(_right);
 
-		if(_currentImage != NULL)
+		if (_currentImage != NULL)
 		{
 			C2D_DrawImageAt(*_currentImage, 0, 0, 0);
 			_currentImage = NULL;
 		}
 
 		float slider = osGet3DSliderState();
-		while(!Triangles.empty())
+		while (!Triangles.empty())
 		{
 			Triangle triangle = Triangles.front();
 			Triangles.pop();
 			int xShift = 0;
 
-			switch(triangle.layer)
+			switch (triangle.layer)
 			{
-				case LAYER_DEFAULT:
-					xShift = 0;
-					break;
-				case LAYER_0:
-					xShift = 4;
-					break;
-				case LAYER_FRONT:
-					xShift = -3;
-					break;
-				case LAYER_UI:
-					xShift = -4;
-					break;
-				default:
-					xShift = 0;
-					break;
+			case LAYER_DEFAULT:
+				xShift = 0;
+				break;
+			case LAYER_0:
+				xShift = 4;
+				break;
+			case LAYER_FRONT:
+				xShift = -3;
+				break;
+			case LAYER_UI:
+				xShift = -4;
+				break;
+			default:
+				xShift = 0;
+				break;
 			}
-			
-			if(slider > 0.0f)
+
+			if (slider > 0.0f)
 			{
 				xShift = xShift * slider;
-				C2D_DrawTriangle(triangle.x0 + xShift, triangle.y0, triangle.color, 
-								triangle.x1 + xShift, triangle.y1, triangle.color,
-								triangle.x2 + xShift, triangle.y2, triangle.color, 0);
+				C2D_DrawTriangle(triangle.x0 + xShift, triangle.y0, triangle.color,
+								 triangle.x1 + xShift, triangle.y1, triangle.color,
+								 triangle.x2 + xShift, triangle.y2, triangle.color, 0);
 			}
 		}
 	}
@@ -357,7 +361,7 @@ void CitroRenderer::resetMatrix()
 
 void CitroRenderer::setClearColor(float red, float green, float blue, float alpha)
 {
-    _clearColor = C2D_Color32f(red, green, blue, alpha);
+	_clearColor = C2D_Color32f(red, green, blue, alpha);
 }
 
 void CitroRenderer::setColor(float color[3])
--- source/3ds/citro_renderer.h
+++ source/3ds/citro_renderer.h
@@ -4,80 +4,199 @@
 #include <citro2d.h>
 #include "../renderer.h"
 
-class CitroRenderer: public Renderer
+/**
+ * Encapsulates all of the 3DS code for rendering graphics.
+ */
+class CitroRenderer : public Renderer
 {
 public:
-
     static const int ScreenHeight;
     static const int ScreenWidth;
 
-   CitroRenderer();
+    CitroRenderer();
 
-   virtual ~CitroRenderer() {}
+    virtual ~CitroRenderer() {}
 
+    /**
+     * Needs to be called before any rendering has begun
+     */
     virtual void beginRendering();
+
+    /**
+     * Clears the screen with the clear color that has been set by setClearColor.
+     * @param includeDepthBuffer - Unused for the 3DS
+     */
     virtual void clearBuffer(bool includeDepthBuffer = false);
 
+    /**
+     * Must be called before exiting to clean up the graphics libraries
+     */
     virtual void deinitialize();
 
+    /**
+     * Draws the keyboard image onto the screen and highlights the specified key.
+     * @param key - The key to highlight
+     */
     virtual void drawKeyboard(struct kbdKey key);
-    
+
+    /**
+     * Draws a line on the screen with the color that has been set by setColor.
+     * @param x0 - The x coordinate for the first point.
+     * @param y0 - The y coordinate for the first point.
+     * @param x1 - The x coordinate for the second point.
+     * @param y1 - The y coordinate for the second point.
+     * @param layer - The layer to draw on (Used for stereoscopic 3D)
+     */
     virtual void drawLine(float x0, float y0, float x1, float y1, Layers layer = LAYER_DEFAULT);
 
+    /**
+     * Draws a quad on the screen filled with the color that has been set by setColor.
+     * @param x0 - The x coordinate for the first point.
+     * @param y0 - The y coordinate for the first point.
+     * @param x1 - The x coordinate for the second point.
+     * @param y1 - The y coordinate for the second point.
+     * @param x2 - The x coordinate for the third point.
+     * @param y2 - The y coordinate for the third point.
+     * @param x3 - The x coordinate for the fourth point.
+     * @param y3 - The y coordinate for the fourth point.
+     * @param layer - The layer to draw on (Used for stereoscopic 3D)
+     */
     virtual void drawQuad(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3,
-                        Layers layer = LAYER_DEFAULT);
+                          Layers layer = LAYER_DEFAULT);
 
+    /**
+     * Draws a vector on the screen with the color that has been set by setColor based on the mode that it is
+     * in. Vector mode draws a line while the other modes draw pixels.
+     * @param x0 - The x coordinate for the first point.
+     * @param y0 - The y coordinate for the first point.
+     * @param x1 - The x coordinate for the second point.
+     * @param y1 - The y coordinate for the second point.
+     * @param layer - The layer to draw on (Used for stereoscopic 3D)
+     */
     virtual void drawVector(float X0, float Y0, float X1, float Y1, Layers layer = LAYER_DEFAULT);
 
+    /**
+     * Must be called after rendering has completed.
+     */
     virtual void endRendering();
 
+    /**
+     * Returns the screen height.
+     */
     virtual int getScreenHeight()
     {
         return ScreenHeight;
     }
 
+    /**
+     * Returns the screen width.
+     */
     virtual int getScreenWidth()
     {
         return ScreenWidth;
     }
 
+    /**
+     * Wrapper function to get flags for SDL. Unused for the 3DS.
+     */
     virtual unsigned int getVideoModeFlags()
     {
         return 0;
     }
 
+    /**
+     * Must be called before use to initialize any libraries.
+     */
     virtual void initialize();
 
+    /**
+     * Plots a pixel based on the specified x and y.
+     * @param X - The x coordinate for the point
+     * @param Y - The y coordinate for the point
+     * @param layer - The layer to draw on (Used for stereoscopic 3D)
+     */
     virtual void plotPoint(double X, double Y, Layers layer = LAYER_DEFAULT);
 
+    /**
+     * Resets any translations that have been set for rendering quads.
+     */
     virtual void resetMatrix();
 
+    /**
+     * Sets the clear color to be used.
+     */
     virtual void setClearColor(float red, float green, float blue, float alpha = 1.0f);
 
+    /**
+     * Sets the color to be used for drawing. Wrapper function to take in an array.
+     */
     virtual void setColor(float color[3]);
+
+    /**
+     * Sets the color to be used for drawing.
+     */
     virtual void setColor(float red, float green, float blue, float alpha = 1.0f);
+
+    /**
+     * Sets the viewport of the camera.
+     */
     virtual void setViewport(int x, int y, int width, int height);
 
+    /**
+     * Called to draw the graphics on screen.
+     */
     virtual void swapBuffers();
 
+    /**
+     * Applies a translation for rendering quads.
+     */
     virtual void translateMatrix(float xOffset, float yOffset);
-private:
 
+private:
+    /**
+     * Helper function to redraw all graphics on the right screen for stereoscopic mode.
+     * It draws 
+     */
     void renderRightScreen();
 
+    /**
+     * The color to clear the screen with.
+     */
     u32 _clearColor;
 
+    /**
+     * The color to draw with.
+     */
     u32 _color;
 
-    C2D_Image* _currentImage;
+    /**
+     * If not NULL then it tells the right screen to render the image.
+     */
+    C2D_Image *_currentImage;
 
+    /**
+     * The keyboard image used by drawKeyboard.
+     */
     C2D_Image _keyboardImg;
 
-    C3D_RenderTarget* _left;
+    /**
+     * The left screen target.
+     */
+    C3D_RenderTarget *_left;
 
-    C3D_RenderTarget* _right;
+    /**
+     * The right screen target for 3D mode.
+     */
+    C3D_RenderTarget *_right;
 
+    /**
+     * x offset for translation.
+     */
     float _xOffset;
+
+    /**
+     * y offset for translation.
+     */
     float _yOffset;
 };
 #endif // DOD_CITRO_RENDERER_HEADER
