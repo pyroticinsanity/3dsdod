b5dc47e1bf55499f125dba5882620036d4a8ba0d
pyroticinsanity <this_can_only_be_mine@hotmail.com>
2020-04-09 00:04:22 +0000

    Adding device specific guards.

--- source/renderer_factory.cpp
+++ source/renderer_factory.cpp
@@ -1,6 +1,8 @@
 #include "renderer_factory.h"
 
+#ifdef _3DS
 #include "3ds/citro_renderer.h"
+#endif
 
 Renderer* RendererFactory::_renderer = NULL;
 RendererFactory::RendererFactory()
@@ -10,7 +12,9 @@ Renderer* RendererFactory::GetRenderer()
 {
     if(_renderer == NULL)
     {
+#ifdef _3DS
         _renderer = new CitroRenderer();
+#endif
     }
 
     return _renderer;
