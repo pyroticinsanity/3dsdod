94f71e7b7996e94b245f5e3d95b9d3f8abe0e1fc
pyroticinsanity <this_can_only_be_mine@hotmail.com>
2020-05-20 03:43:36 +0000

    adding a test for mtime comparison

---
+++ test/mtime/README.md
@@ -0,0 +1,6 @@
+Modified Time Example
+=======
+
+This example compares the modified time from stat to archive_getmtime of a file in /3ds/test.txt
+Controls:
+- Start - Exit the homebrew
---
+++ test/mtime/source/main.c
@@ -0,0 +1,91 @@
+// Comparison of getting write time from stat VS archive_getmtime
+
+#include <3ds.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void waitForExit()
+{
+	printf("Press START to Exit\n");
+	while (aptMainLoop())
+	{
+		svcSleepThread(1);
+		hidScanInput();
+
+		// Respond to user input
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+	}
+}
+
+//---------------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
+//---------------------------------------------------------------------------------
+	// Init libs
+	gfxInitDefault();
+	consoleInit(GFX_BOTTOM, NULL);
+
+	const char* filename = "/3ds/test.txt";
+
+	FILE* file = fopen(filename, "w");
+
+	if(file == NULL)
+	{
+		printf("Failed to open file\n");
+		waitForExit();
+	}
+
+	fprintf(file, "Testing!\n");
+
+	fclose(file);
+
+
+	struct stat stats;
+	struct tm* time;
+	u64 mtime;
+	char date[50];
+
+	int error = stat(filename, &stats);
+
+	if(error != 0)
+	{
+		printf("Failed to stat!\n");
+		waitForExit();
+	}
+
+	error = archive_getmtime(filename, &mtime);
+
+	if(error != 0)
+	{
+		printf("failed to get mtime\n");
+		waitForExit();
+	}
+
+	time = localtime(&stats.st_mtime);
+	strftime(date, sizeof(date), "%Y.%m.%d %I:%M %p", time);
+
+	// piepie62 confirmed that stat does nothing to mtime
+	printf("Stat: %s\n", date);
+
+	stats.st_mtime = mtime;
+	time = localtime(&stats.st_mtime);
+	strftime(date, sizeof(date), "%Y.%m.%d %I:%M %p", time);
+	
+	printf("Archive_getmtime: %s\n", date);
+
+	// Main loop
+	waitForExit();
+
+	// Deinit libs
+	gfxExit();
+	return 0;
+}
+
+
+
+
